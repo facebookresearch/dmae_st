db435aa46d2d5f50fb31c852ab99c400be2722af
litesaber15 <kart@fb.com>
2023-01-18 17:54:37 +0000

    dir changes in finetune 10% scripts

--- scripts/2022-12-07:finetune-fixed_seeding.sh
+++ scripts/2022-12-07:finetune-fixed_seeding.sh
@@ -1,10 +1,12 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
 
+OUT_DIR="experiments/dmae-st/finetune_10pct"
+
 # Both versions of MAE_ST at 10%
-python src/mae_submitit_finetune.py --job_dir="experiments/mae-st/8x8A100-MAE_ST-bs3-fp32-EFA/finetune-fixed_frac010_sample" \
-    --nodes=4 --ngpus=8 --exclude="" \
-    --partition=lowpri --account=all \
+python src/mae_submitit_finetune.py --job_dir="$OUT_DIR/8x8A100-MAE_ST-bs3-fp32-EFA" \
+    --nodes=4 --num_gpus=8 --exclude="" \
+    --partition=learnai4p --account=all \
     --distributed \
     --epochs=100 --warmup_epochs=10 --dist_eval --checkpoint_period=10 \
     --batch_size=3 --repeat_aug=1 --accum_iter=1 \
@@ -19,12 +21,12 @@ python src/mae_submitit_finetune.py --job_dir="experiments/mae-st/8x8A100-MAE_ST
     --encoder_attn=AttentionWithCls \
     --cls_embed --sep_pos_embed --clip_grad=5.0 --fp32 \
     --finetune_fraction=0.1 \
-    --finetune="experiments/mae-st/8x8A100-MAE_ST-bs3-fp32-EFA/checkpoint-00199.pth"
+    --finetune="/fsx/alexnw/experiments/mae-st/8x8A100-MAE_ST-bs3-fp32-EFA/checkpoint-00199.pth"
 
 # This is the second version with K400v2 -- idr how they're diff.
-python src/mae_submitit_finetune.py --job_dir="experiments/mae-st/MAE_ST-200epochs-k400v2-288px/finetune-fixed_frac010_sample" \
-    --nodes=4 --ngpus=8 --exclude="" \
-    --partition=lowpri --account=all \
+python src/mae_submitit_finetune.py --job_dir="$OUT_DIR/MAE_ST-200epochs-k400v2-288px" \
+    --nodes=4 --num_gpus=8 --exclude="" \
+    --partition=learnai4p --account=all \
     --distributed \
     --epochs=100 --warmup_epochs=10 --dist_eval --checkpoint_period=10 \
     --batch_size=3 --repeat_aug=1 --accum_iter=1 \
@@ -39,12 +41,12 @@ python src/mae_submitit_finetune.py --job_dir="experiments/mae-st/MAE_ST-200epoc
     --encoder_attn=AttentionWithCls \
     --cls_embed --sep_pos_embed --clip_grad=5.0 --fp32 \
     --finetune_fraction=0.1 \
-    --finetune="experiments/mae-st/MAE_ST-200epochs-k400v2-288px/checkpoints/checkpoint-00199.pth"
+    --finetune="/fsx/alexnw/experiments/mae-st/MAE_ST-200epochs-k400v2-288px/checkpoints/checkpoint-00199.pth"
 
 # This should be the curriculum model that got the best results.
-python src/mae_submitit_finetune.py --job_dir="experiments/dmae-st/DMAE_ST-200epochs-TempCos150_050/finetune-fixed_frac010_sample" \
-    --ngpus=8 --nodes=4 --exclude="" \
-    --partition=lowpri --account=all \
+python src/mae_submitit_finetune.py --job_dir="$OUT_DIR/DMAE_ST-200epochs-TempCos150_050" \
+    --num_gpus=8 --nodes=4 --exclude="" \
+    --partition=learnai4p --account=all \
     --distributed \
     --epochs=100 --warmup_epochs=10 --dist_eval --checkpoint_period=10 \
     --batch_size=3 --repeat_aug=1 --accum_iter=1 \
@@ -59,13 +61,13 @@ python src/mae_submitit_finetune.py --job_dir="experiments/dmae-st/DMAE_ST-200ep
     --encoder_attn=AttentionWithCls \
     --cls_embed --sep_pos_embed --clip_grad=5.0 --fp32 \
     --finetune_fraction=0.1 \
-    --finetune="experiments/dmae-st/DMAE_ST-200epochs-TempCos150_050/checkpoint-00199.pth"
+    --finetune="/fsx/alexnw/experiments/dmae-st/DMAE_ST-200epochs-TempCos150_050/checkpoint-00199.pth"
 
 # This is the first half of the sweep results.
 for TEMP in -1e10 -2e0 -1e0 -0.5e0 0; do
-    python src/mae_submitit_finetune.py --job_dir="experiments/dmae-st/DMAE_ST-full-sweep/DMAE_ST-full-TEMP$TEMP/finetune-fixed_frac010_sample" \
-        --ngpus=8 --nodes=4 --exclude="" \
-        --partition=lowpri --account=all \
+    python src/mae_submitit_finetune.py --job_dir="$OUT_DIR/DMAE_ST-full-TEMP$TEMP" \
+        --num_gpus=8 --nodes=4 --exclude="" \
+        --partition=learnai4p --account=all \
         --distributed \
         --epochs=100 --warmup_epochs=10 --dist_eval --checkpoint_period=10 \
         --batch_size=3 --repeat_aug=1 --accum_iter=1 \
@@ -80,5 +82,5 @@ for TEMP in -1e10 -2e0 -1e0 -0.5e0 0; do
         --encoder_attn=AttentionWithCls \
         --cls_embed --sep_pos_embed --clip_grad=5.0 --fp32 \
         --finetune_fraction=0.1 \
-        --finetune="experiments/dmae-st/DMAE_ST-full-sweep/DMAE_ST-full-TEMP$TEMP/checkpoints/checkpoint-00199.pth" # extra /checkpoints/ folder
+        --finetune="/fsx/kart/skel_act_recg/experiments/dmae-st/DMAE_ST-full-sweep/DMAE_ST-full-TEMP$TEMP/checkpoints/checkpoint-00199.pth" # extra /checkpoints/ folder
 done

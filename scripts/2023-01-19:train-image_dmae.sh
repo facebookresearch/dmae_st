86ec5233e78768285b4e07c27c4500207e3750db
Alex <anw2067@nyu.edu>
2023-01-19 19:05:30 -0500

    Scripts

---
+++ scripts/2023-01-19:train-image_dmae.sh
@@ -0,0 +1,19 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+IMAGENET_DIR=""
+IMAGENET_BBOX_DIR=""
+
+# Both versions of MAE_ST at 10%
+python src/image_mae_submitit_pretrain.py --job_dir="experiments/image_dmae/train" \
+    --nodes=8 --exclude="" \
+    --partition=learnai4p --acount=all \
+    --distributed \
+    --epochs=800 --warmup_epochs=40 \
+    --batch_size=64 --accum_iter=1 \
+    --model mae_vit_large_patch16 \
+    --norm_pix_loss \
+    --mask_ratio=0.75 --temperature=1.0 \
+    --blr=1.5e-4 --weight_decay=0.05 \
+    --data_path=$IMAGENET_DIR \
+    --bbox_path=$IMAGENET_BBOX_DIR
